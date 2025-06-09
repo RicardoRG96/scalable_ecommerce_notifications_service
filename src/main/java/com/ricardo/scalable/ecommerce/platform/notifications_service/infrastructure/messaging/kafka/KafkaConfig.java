fe73f220508cae249dc75a1e281c76802c8f1044
RicardoRG96 <ricardoretamal10@gmail.com>
2025-06-09 05:56:13 -0400

    add KafkaConfig class

---
+++ src/main/java/com/ricardo/scalable/ecommerce/platform/notifications_service/infrastructure/messaging/kafka/KafkaConfig.java
@@ -0,0 +1,46 @@
+package com.ricardo.scalable.ecommerce.platform.notifications_service.infrastructure.messaging.kafka;
+
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+@Configuration
+@EnableKafka
+public class KafkaConfig {
+
+    private final KafkaProperties kafkaProperties;
+
+    public KafkaConfig(KafkaProperties kafkaProperties) {
+        this.kafkaProperties = kafkaProperties;
+    }
+
+    @Bean
+    ConsumerFactory<String, Object> consumerFactory() {
+        JsonDeserializer<Object> deserializer = new JsonDeserializer<>();
+        deserializer.setRemoveTypeHeaders(false);
+        deserializer.addTrustedPackages("*"); // mejor especificar paquetes por seguridad
+        deserializer.setUseTypeMapperForKey(true);
+
+        return new DefaultKafkaConsumerFactory<String, Object>(
+            kafkaProperties.buildConsumerProperties(),
+            new StringDeserializer(),
+            deserializer
+        );
+    }
+
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+                
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+
+}
