9d91a1a70dc19180bd86757e8d18d58b4a03f5a0
RicardoRG96 <ricardoretamal10@gmail.com>
2025-06-11 06:35:21 -0400

    add logger to event listener classes

--- src/main/java/com/ricardo/scalable/ecommerce/platform/notifications_service/application/listeners/UserBirthdayEventListener.java
+++ src/main/java/com/ricardo/scalable/ecommerce/platform/notifications_service/application/listeners/UserBirthdayEventListener.java
@@ -1,5 +1,7 @@
 package com.ricardo.scalable.ecommerce.platform.notifications_service.application.listeners;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.ricardo.scalable.ecommerce.platform.libs_common.events.UserBirthdayEvent;
@@ -8,11 +10,16 @@ import com.ricardo.scalable.ecommerce.platform.notifications_service.messaging.E
 @Component
 public class UserBirthdayEventListener implements EventListener<UserBirthdayEvent> {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserBirthdayEventListener.class);
+
     @Override
     public void onEvent(UserBirthdayEvent event) {
         // Handle the user birthday event
         // send birthday wishes, etc.
-        System.out.println("User birthday: " + event.getUserId());
+        logger.info("User birthday: {}", event.getUserId());
+        logger.info("User email: {}", event.getEmail());
+        logger.info("User username: {}", event.getName());
+        logger.info("User birthday date: {}", event.getBirthday());
     }
 
 }
--- src/main/java/com/ricardo/scalable/ecommerce/platform/notifications_service/application/listeners/UserRegisteredEventListener.java
+++ src/main/java/com/ricardo/scalable/ecommerce/platform/notifications_service/application/listeners/UserRegisteredEventListener.java
@@ -1,5 +1,7 @@
 package com.ricardo.scalable.ecommerce.platform.notifications_service.application.listeners;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.ricardo.scalable.ecommerce.platform.libs_common.events.UserRegisteredEvent;
@@ -8,11 +10,15 @@ import com.ricardo.scalable.ecommerce.platform.notifications_service.messaging.E
 @Component
 public class UserRegisteredEventListener implements EventListener<UserRegisteredEvent> {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserRegisteredEventListener.class);
+
     @Override
     public void onEvent(UserRegisteredEvent event) {
         // Handle the user registered event
         // send welcome email, etc.
-        System.out.println("User registered: " + event.getUserId());
+        logger.info("User registered: {}", event.getUserId());
+        logger.info("User email: {}", event.getEmail());
+        logger.info("User username: {}", event.getName());
     }
 
 }
