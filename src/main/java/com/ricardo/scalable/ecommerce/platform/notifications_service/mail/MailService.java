e20c4086709e33f19a000e3df78ceff268124508
RicardoRG96 <ricardoretamal10@gmail.com>
2025-06-18 06:43:49 -0400

    add EmailRequest DTO

--- src/main/java/com/ricardo/scalable/ecommerce/platform/notifications_service/mail/MailService.java
+++ src/main/java/com/ricardo/scalable/ecommerce/platform/notifications_service/mail/MailService.java
@@ -1,5 +1,7 @@
 package com.ricardo.scalable.ecommerce.platform.notifications_service.mail;
 
+import com.ricardo.scalable.ecommerce.platform.notifications_service.mail.dto.EmailRequest;
+
 public interface MailService {
 
     void sendEmail(EmailRequest request);
