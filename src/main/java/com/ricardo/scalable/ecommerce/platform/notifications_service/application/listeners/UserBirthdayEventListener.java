8f19ea2a61783af9acbc6e47e69979cedd5ccdf4
RicardoRG96 <ricardoretamal10@gmail.com>
2025-06-20 08:45:43 -0400

    add the email sending implementation to UserBirthdayEventListener

--- src/main/java/com/ricardo/scalable/ecommerce/platform/notifications_service/application/listeners/UserBirthdayEventListener.java
+++ src/main/java/com/ricardo/scalable/ecommerce/platform/notifications_service/application/listeners/UserBirthdayEventListener.java
@@ -2,24 +2,39 @@ package com.ricardo.scalable.ecommerce.platform.notifications_service.applicatio
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.ricardo.scalable.ecommerce.platform.libs_common.events.UserBirthdayEvent;
+import com.ricardo.scalable.ecommerce.platform.notifications_service.mail.MailService;
+import com.ricardo.scalable.ecommerce.platform.notifications_service.mail.dto.EmailRequest;
 import com.ricardo.scalable.ecommerce.platform.notifications_service.messaging.EventListener;
 
 @Component
 public class UserBirthdayEventListener implements EventListener<UserBirthdayEvent> {
 
+    @Autowired
+    private MailService mailService;
+
     private static final Logger logger = LoggerFactory.getLogger(UserBirthdayEventListener.class);
 
     @Override
     public void onEvent(UserBirthdayEvent event) {
-        // Handle the user birthday event
-        // send birthday wishes, etc.
-        logger.info("User birthday: {}", event.getUserId());
         logger.info("User email: {}", event.getEmail());
         logger.info("User username: {}", event.getName());
-        logger.info("User birthday date: {}", event.getBirthday());
+
+        EmailRequest email = new EmailRequest();
+        email.setTo(event.getEmail());
+        email.setSubject("Happy Birthday from Scalable E-commerce Platform");
+        email.setBody(
+            "Hello " + event.getName() + ",\n\n" +
+            "Wishing you a very happy birthday! We hope you have a wonderful day filled with joy and celebration.\n\n" +
+            "Best wishes,\n" +
+            "Scalable E-commerce Team"
+        );
+
+        mailService.sendEmail(email);
+        logger.info("Birthday email sent to {}", event.getEmail());
     }
 
 }
