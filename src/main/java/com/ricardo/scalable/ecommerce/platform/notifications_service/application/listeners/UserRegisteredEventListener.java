4fdbfc5b56fa011331b026a855b870328471c3c2
RicardoRG96 <ricardoretamal10@gmail.com>
2025-06-20 06:56:56 -0400

    add the email sending implementation to UserRegisteredEventListener

--- src/main/java/com/ricardo/scalable/ecommerce/platform/notifications_service/application/listeners/UserRegisteredEventListener.java
+++ src/main/java/com/ricardo/scalable/ecommerce/platform/notifications_service/application/listeners/UserRegisteredEventListener.java
@@ -2,23 +2,38 @@ package com.ricardo.scalable.ecommerce.platform.notifications_service.applicatio
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.ricardo.scalable.ecommerce.platform.libs_common.events.UserRegisteredEvent;
+import com.ricardo.scalable.ecommerce.platform.notifications_service.mail.MailService;
+import com.ricardo.scalable.ecommerce.platform.notifications_service.mail.dto.EmailRequest;
 import com.ricardo.scalable.ecommerce.platform.notifications_service.messaging.EventListener;
 
 @Component
 public class UserRegisteredEventListener implements EventListener<UserRegisteredEvent> {
 
+    @Autowired
+    private MailService mailService;
+
     private static final Logger logger = LoggerFactory.getLogger(UserRegisteredEventListener.class);
 
     @Override
     public void onEvent(UserRegisteredEvent event) {
-        // Handle the user registered event
-        // send welcome email, etc.
-        logger.info("User registered: {}", event.getUserId());
         logger.info("User email: {}", event.getEmail());
         logger.info("User username: {}", event.getName());
+
+        EmailRequest email = new EmailRequest();
+        email.setTo(event.getEmail());
+        email.setSubject("Welcome to Scalable E-commerce Platform");
+        email.setBody(
+            "Hello " + event.getName() + ",\n\n" +
+            "Thank you for registering on our platform! We are excited to have you with us.\n\n" +
+            "Best regards,\n" +
+            "Scalable E-commerce Team"
+        );
+
+        mailService.sendEmail(email);
     }
 
 }
